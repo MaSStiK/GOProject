68ed4a09a4de528aef0be119a91341ca51c5fd91
Овчинников Матвей Яковлевич <pl8394@C217N11.pl1.edu>
2022-04-23 13:51:24 +0500

    add

--- go.mod
+++ go.mod
@@ -1,24 +1,31 @@
-module server
+module notice
 
-go 1.18
+go 1.17
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.7 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.1 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
-	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/go-pg/pg/v10 v10.10.6 // indirect
+	github.com/go-pg/zerochecker v0.2.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/vmihailenco/bufpool v0.1.11 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser v0.1.2 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20210923061019-b8560ed6a9b7 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,62 +1,132 @@
 package main
 
-import(
-	"database/sql"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-	"encoding/json"
+	"github.com/go-pg/pg/v10"
 )
 
-type list struct{
-	id int `json:"id"`
-	info string `json:"info"`
+type Note struct {
+	tableName struct{} `pg:"notes"`
+	Id        int64    `json:"id" pg:"id,pk"`
+	CreatedAt string   `json:"created_at" pg:"created_at"`
+	Title     string   `json:"title" pg:"title"`
+	Info      string   `json:"info" pg:"info"`
 }
 
-func api(c *gin.Context) {
-	db, err := sql.Open("mysql", "root:root@/test")
+func SelectNotes() []Note {
+	var notes []Note
+	db := pgDataBase()
+
+	err := db.Model(&notes).Select()
 
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
-	db.Exec("insert into test.list (info) value (?)", "Hello world!")
+	db.Close()
+
+	return notes
+}
+
+func SelectNote(id int64) Note{
+	var note Note
+	db := pgDataBase()
+
+	err := db.Model(&note).Where("id = ?", id).Select()
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+
+	return note
+}
+
+func InsertNote(note Note) Note {
+	db := pgDataBase()
+
+	_, err := db.Model(&note).Insert()
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+
+	return note
+}
+
+func pgDataBase() *pg.DB {
+	address := fmt.Sprintf("%s:%s", "localhost", "5432")
+	options := &pg.Options{
+		Addr:     address,
+		User:     "postgres",
+		Password: "postgres",
+		Database: "notice",
+		PoolSize: 50,
+	}
+
+	con := pg.Connect(options)
+
+	if con == nil {
+		log.Fatal("Нет подключения к БД")
+	}
+	return con
+}
+
+func Api(c *gin.Context) {
+	db := pgDataBase()
+
+	db.Close()
 
 	c.JSON(200, gin.H{
-		"message": "1",
+		"api": "notice",
 	})
 }
 
-func all(c *gin.Context) {
-	db, err := sql.Open("mysql", "root:root@/test")
+func GetNotes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, SelectNotes())
+}
 
+func AddNote(c *gin.Context) {
+	var note Note
+
+	err := c.BindJSON(&note)
 	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("select * from test.list")
-
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	lists := []list{}
-
-	for rows.Next() {
-		l := list{}
-		rows.Scan(&l.id, &l.info)
-		lists = append(lists, l)
+		return
 	}
 
-	b, _ := json.Marshal(lists)
+	// note.CreatedAt = time.Now().String()
 
-	c.JSON(200, string(b))
+	note.CreatedAt = "2022-04-23T00:00:00"
+	// note.CreatedAt = time.Now().UTC().String()
+
+	c.IndentedJSON(http.StatusOK, InsertNote(note))
+}
+
+func GetNote(c *gin.Context) {
+	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+
+	c.IndentedJSON(http.StatusOK, SelectNote(id))
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/api", api)
-	r.GET("/all", all)
+
+	rAPI := r.Group("/api")
+	{
+		rAPI.GET("/notes", GetNotes)
+		rNote := rAPI.Group("/note")
+		{
+			rNote.POST("/add", AddNote)
+			rNote.GET("/:id", GetNote)
+		}
+	}
 	r.Run("0.0.0.0:9090")
 }
